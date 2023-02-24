7da7d9e9836662f9bec3c267db378fec9a81f269
AbeetoChan <abinavkumar347@gmail.com>
2023-02-23 21:53:30 -0500

    Cleanup code

--- src/leveling.py
+++ src/leveling.py
@@ -1,7 +1,9 @@
+import asyncio
+
 import discord
 from discord.ext import commands
+
 from config import mongo_client, STRIKES_BEFORE_BAN
-import asyncio
 
 with open("swear_words.txt") as s:
     SWEAR_WORDS = [sw.lower().replace("\n", "") for sw in s.readlines()]
@@ -10,10 +12,10 @@ with open("swear_words.txt") as s:
 class Leveling(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.users = mongo_client["user_db"]["user_col"]
+        self.users = mongo_client["user_db"]["users"]
        
     def handle_member_join(self, user_id):
-        x = self.users.insert_one({
+        self.users.insert_one({
             "user_id": user_id,
             "pts": 0, 
             "lvl": 0, 
@@ -30,10 +32,14 @@ class Leveling(commands.Cog):
         if not self.user_exists(user_id):
             self.handle_member_join(user_id)
 
+    def get_user_id(self, user):
+        user_id = user.id
+        self.handle_nonexistent_user_in_db(user_id)
+        return user_id
+
     @commands.slash_command(description="See how many swear strikes a user has")
     @commands.has_permissions(administrator=True)
     @commands.guild_only()
-    @discord.option("user", str, description="The user you want to see the swear strikes of")
     async def see_swear_strikes(self, ctx, user):
         user = ctx.author.guild.get_member_named(user)
 
@@ -41,19 +47,15 @@ class Leveling(commands.Cog):
             await ctx.respond("It seems like that person does not exist...")
             return            
 
-        user_id = user.id
-        self.handle_nonexistent_user_in_db(user_id)
-
-
-        strikes = self.users.find_one({"user_id": user_id}, {"strikes": 1})["strikes"]       
+        user_id = self.get_user_id(user)
+        strikes = self.users.find_one({"user_id": user_id}, {"strikes": 1})["strikes"]
 
         await ctx.respond(f"{user.display_name} has {strikes} strikes", ephemeral=True)
 
     @commands.slash_command(description="See your level!")
     @commands.guild_only()
     async def level(self, ctx):
-        user_id = ctx.author.id
-        self.handle_nonexistent_user_in_db(user_id)
+        user_id = self.get_user_id(ctx.author)
         level = self.users.find_one({"user_id": user_id}, {"lvl": 1})["lvl"]
 
         embed = discord.Embed(
@@ -69,13 +71,11 @@ class Leveling(commands.Cog):
     @commands.slash_command(description="See this server's level leaderboard!")
     @commands.guild_only()
     async def leaderboard(self, ctx):
-        # Computationally expensive
         users = self.users.find({}, {"user_id": 1})
         users = [user["user_id"] for user in users]
         levels = self.users.find({}, {"lvl": 1})
         levels = [level["lvl"] for level in levels]
         values = list(zip(users, levels))
-
         values.sort(key=lambda k: k[1], reverse=True)
 
         embed = discord.Embed(
@@ -94,21 +94,8 @@ class Leveling(commands.Cog):
             embed.set_author(name=ctx.author.display_name, icon_url=ctx.author.avatar)
 
         embed.set_footer(text="The rankings are subject to change")
+
         await ctx.respond(embed=embed)
-
-    @staticmethod
-    async def level_up(message, new_level):
-        embed = discord.Embed(
-            title="Level up!",
-            description=f"You have leveled up to level {new_level}!",
-            color=discord.Color.gold()
-        )
-
-        if message.author.avatar is None:
-            embed.set_author(name=message.author.display_name)
-        else:
-            embed.set_author(name=message.author.display_name, icon_url=message.author.avatar.url)
-        await message.reply(embed=embed)
 
     @staticmethod
     def can_level_up(current_level, pts):
@@ -116,40 +103,14 @@ class Leveling(commands.Cog):
         return (current_level ** 2) / 45 + 3.3 * current_level < pts
 
     @staticmethod
-    def has_profanity_in_it(content: str):
+    def contains_profanity(content):
         for sw in SWEAR_WORDS:
             if sw in content:
                 return True
 
         return False
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.id == self.bot.user.id:
-            return
-
-        user_id = message.author.id
-
-        self.handle_nonexistent_user_in_db(user_id)
-
-        if self.has_profanity_in_it(message.content):
-            self.users.update_one({"user_id": user_id}, {
-                "$inc": {"strikes": 1}
-            })
-            strikes = self.users.find_one({"user_id": user_id}, {"strikes": 1})["strikes"]
-
-            await message.delete()
-            dm = await message.author.create_dm()
-            await dm.send(f"Please don't send offensive messages like that! Strike #{strikes}")
-
-            if strikes > STRIKES_BEFORE_BAN:
-                await asyncio.sleep(2)
-
-                await dm.send("You have been banned for swearing too much!")
-                await message.author.ban(reason="Too many swear strikes!")
-                
-            return
-
+    async def handle_leveling(self, message, user_id):
         self.users.update_one({"user_id": user_id}, {
             "$inc": {"pts": 1}
         })
@@ -164,9 +125,45 @@ class Leveling(commands.Cog):
             self.users.update_one({"user_id": user_id}, {
                 "$inc": {"lvl": 1}
             })
-            current_level += 1
-            await self.level_up(message, current_level)
 
+            current_level += 1
+            embed = discord.Embed(
+                title="Level up!",
+                description=f"Congrats! You have leveled up to level {current_level}!",
+                color=discord.Color.gold()
+            )
+
+            await message.reply(embed=embed, mention_author=True)
+
+    async def handle_profanity(self, message, user_id):
+        if self.contains_profanity(message.content):
+            self.users.update_one({"user_id": user_id}, {
+                "$inc": {"strikes": 1}
+            })
+            strikes = self.users.find_one({"user_id": user_id}, {"strikes": 1})["strikes"]
+
+            await message.delete()
+
+            dm = await message.author.create_dm()
+            await dm.send(f"Please don't send offensive messages like that! Strike #{strikes}")
+
+            if strikes > STRIKES_BEFORE_BAN:
+                await asyncio.sleep(2)
+
+                await dm.send("You have been banned for swearing too much!")
+                await message.author.ban(reason="Too many swear strikes!")
+
+            return
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.id == self.bot.user.id:
+            return
+
+        user_id = self.get_user_id(message.author)
+
+        await self.handle_profanity(message, user_id)
+        await self.handle_leveling(message, user_id)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
@@ -174,11 +171,9 @@ class Leveling(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_remove(self, member: discord.Member):
-        user_id = member.id
+        self.remove_member(member.id)
 
-        self.remove_member(user_id)
-
-    async def cog_command_error(self, ctx: commands.Context, error: commands.CommandError): # noqa
+    async def cog_command_error(self, ctx, error):  # noqa
         if isinstance(error, commands.MissingPermissions):
             await ctx.respond("It seems like you do not have the permissions to run this command...")
         else:
--- src/main.py
+++ src/main.py
@@ -1,17 +1,12 @@
-import json
 import discord
 import config
 
-#######################
-####################### 
-
-intents: discord.Intents = discord.Intents.default()
+intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 bot = discord.Bot(intents=intents)
 
 bot.load_extension("leveling")
-#bot.load_extension("teacher_review")
 
 bot.run(config.TOKEN)
